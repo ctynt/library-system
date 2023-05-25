1b96afaaa80fd36a506df2769c5db59e8d656879
tyx <2597704619qq.com>
2023-05-25 09:45:49 +0800

    单元格居中及列表位置调整

--- src/main/java/frame/AdminStorageFrame.java
+++ src/main/java/frame/AdminStorageFrame.java
@@ -17,6 +17,7 @@ import java.util.Date;
 import java.util.Vector;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import dao.BookDao;
@@ -86,7 +87,7 @@ public class AdminStorageFrame extends JFrame {
         data = bookDaoImpl.getBookInfo();
         tableModel = new DefaultTableModel(data,header);
         table = new JTable(tableModel);
-        // 创建表格
+//        // 创建表格
         table.getTableHeader().setFont(new Font(null, Font.BOLD, 14));
         // 设置表头名称字体样式
         table.getTableHeader().setForeground(Color.black);
@@ -95,10 +96,14 @@ public class AdminStorageFrame extends JFrame {
         // 设置不允许手动改变列宽
         table.getTableHeader().setReorderingAllowed(false);
         // 设置表头不允许拖动
+        DefaultTableCellRenderer r = new DefaultTableCellRenderer();
+        r.setHorizontalAlignment(JLabel.CENTER);
+        table.setDefaultRenderer(Object.class, r);
+        //单元格居中
         int v = ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
         // 水平滚动条
         int h = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
-        // 垂直滚动条
+//         垂直滚动条
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -106,8 +111,9 @@ public class AdminStorageFrame extends JFrame {
                 new AdminMainFrame().setVisible(true);
             }
         });
+        System.out.println(h);
         JScrollPane jsp = new JScrollPane(table, v, h);
-        jsp.setBounds(26, 105, 778, 131);
+        jsp.setBounds(0, 36, 850, 400);
         this.add(jsp);
 //         新建表格
         tableModel = new DefaultTableModel(rowData, columnNames);
@@ -205,11 +211,7 @@ public class AdminStorageFrame extends JFrame {
         }
 
 
-        public void refresh() {
-            Object[][] data = bookDaoImpl.getBookInfo();
-            tableModel.setDataVector(data, header);
 
-        }
 
         public void MyEvent() {
             button.addActionListener(new ActionListener() {
@@ -387,6 +389,12 @@ public class AdminStorageFrame extends JFrame {
             }
 
         });
+    }
+
+    public void refresh() {
+        data = bookDaoImpl.getBookInfo();
+        tableModel.setDataVector(data, header);
+
     }
 
     public static void main(String[] args) {
